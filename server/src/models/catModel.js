a2d64545678fca5843f2e43375569e630105675e
Himanshu Singh <hs5317610@gmail.com>
2026-07-20 12:36:45 +0530

    Add Cat model schema with validation and default values

---
+++ server/src/models/catModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const catSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    breed: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    energyLevel: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+
+    lifeSpan: {
+      type: Number,
+      required: true,
+      default:1,
+    },
+
+    kidsFriendly: {
+      type: Boolean,
+      default: false,
+    },
+
+    apartmentFriendly: {
+      type: Boolean,
+      default: false,
+    },
+
+    image: {
+      type: String,
+    //   required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Cat", catSchema);
