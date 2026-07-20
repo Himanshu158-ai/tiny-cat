2d4b8e17f72441cc18a9a9c955474da3b848585a
Himanshu Singh <hs5317610@gmail.com>
2026-07-20 12:29:36 +0530

    Refactor server initialization and add MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import app from './src/app.js'
 import dotenv from 'dotenv'
 dotenv.config();
-
-
+import connectDB from './src/config/db.js';
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
+
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
---
+++ server/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Mongo Connected");
+    } catch (error) {
+        console.log("error in mongoDB",error);
+    }
+}
+
+export default connectDB;
